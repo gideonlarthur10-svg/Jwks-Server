7d324f7b0db4a998903b8527876e39ba73f5f2ff
gideonlarthur10-svg <gideonlarthur10@gmail.com>
2025-09-20 17:45:00 -0500

    Delete util.rs

--- util.rs
+++
@@ -1,6 +0,0 @@
-use base64::{engine::general_purpose, Engine as _};
-
-/// Base64 URL-safe without padding
-pub fn base64url(bytes: &[u8]) -> String {
-    general_purpose::URL_SAFE_NO_PAD.encode(bytes)
-}
